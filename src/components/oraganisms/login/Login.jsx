0ecec473cf7d64b74386d329505364c96ef974ce
Edgar <jorge260590@hotmail.com>
2023-12-06 18:35:50 -0500

    ci(login switch): Add new implementation in login to switch to signup

--- src/components/oraganisms/login/Login.jsx
+++ src/components/oraganisms/login/Login.jsx
@@ -1,16 +1,37 @@
 import './Login.scss';
 import { LoginContainer } from '../../index';
+import {useState} from 'react'
+
 const Login = () => {
+	const [showLog, setShowLog] = useState(true);
+	const [showSign, setShowSign] = useState(false);
+
+	const handleListFunctions = {
+		log: () => {
+			setShowLog(true)
+			setShowSign(false)
+		},
+		sign: () => {
+			setShowLog(false)
+			setShowSign(true)
+		}
+	}
+
+	const handleClick = key => e => {
+		e.preventDefault()
+		const handleFunctions = handleListFunctions[key];
+		handleFunctions && handleFunctions();
+	}
 	return (
 		<div className='login__container'>
 			<figure className='login__img'>
-				<button className='login__switch title--m'>Log-In</button>
-				<button className='login__switch title--m disactive'>Sign-Up</button>
+				<button className={`login__switch title--m ${showLog ? '':'disactive'}`} onClick={handleClick('log')}>Log-In</button>
+				<button className={`login__switch title--m ${showSign ? '':'disactive'}`} onClick={handleClick('sign')}>Sign-Up</button>
 			</figure>
-			<LoginContainer buttonText={'Log-In'} logOrSig={'Log-In'} />
-			<LoginContainer className={'hide'} />
+			<LoginContainer className={showLog ? '':'hide'} buttonText={'Log-In'} logOrSig={'Log-In'} />
+			<LoginContainer className={showSign ? '':'hide'} buttonText={'Sign-Up'} logOrSig={'Sign-Up'}/>
 		</div>
 	);
 };
 
-export default Login;
+export default Login
