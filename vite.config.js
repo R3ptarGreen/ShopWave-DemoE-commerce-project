f71ac95b28dafe270ac16896e19b1bd50d1acab4
Edgar <jorge260590@hotmail.com>
2023-12-11 16:52:49 -0500

    ready to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "https://r3ptargreen.github.io/ShopWave-DemoE-commerce-project/",
 	css: {
 		preprocessorOptions: {
 			scss: {
